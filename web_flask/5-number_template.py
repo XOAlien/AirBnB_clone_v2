6c701c420f1dbe24ca76d8938744a0e4b5cd2035
Omar Cherni <89669057+XOAlien@users.noreply.github.com>
2022-06-11 16:56:53 +0100

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,49 +1,47 @@
 #!/usr/bin/python3
-'''prints number if its integer'''
-from flask import Flask
-from flask import render_template
+"""
+starts a Flask web application
+"""
+
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_route():
-    '''prints Hello HBNB'''
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    '''print HBNB'''
+    """returns HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    '''prints C followed by input text'''
-    c_text = 'C {}'.format(text)
-    c_text = c_text.replace('_', ' ')
-    return c_text
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def py_route(text='is cool'):
-    '''prints Python followed by input text'''
-    py_text = 'Python {}'.format(text)
-    py_text = py_text.replace('_', ' ')
-    return py_text
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def num_route(n):
-    '''prints number <n> if it is an integer'''
-    return '{} is a number'.format(n)
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def num_template(n):
-    '''displays HTML page if <n> is an integer'''
-    return render_template('5-number.html', num=n)
+def numbersandtemplates(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
